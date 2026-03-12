fa1beb0b097c2c3ad6e168f2d7348fc0aaab7d46
Anmir12 <skannu30@gmail.com>
2026-03-12 23:21:17 +0530

    pattern 5 revisit

--- PatternPrinting/Pattern5.js
+++ PatternPrinting/Pattern5.js
@@ -11,12 +11,13 @@
 function Pattern5(n) {
   for (let row = 1; row <= n; row++) {
     let str = "";
-    let stars = n-row + 1
-    for (let col = 1; col <= stars; col++) {
+    let stars = n - row + 1;
+    for (let j = 1; j <= stars; j++) {
       str += "*";
     }
+
     console.log(str);
   }
 }
 
-Pattern5(5);
+Pattern5(6);
