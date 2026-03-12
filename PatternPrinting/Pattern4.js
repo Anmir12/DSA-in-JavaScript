e3461def48b2203a7efd51d307a7a8999a833fda
Anmir12 <skannu30@gmail.com>
2026-03-12 22:59:13 +0530

    pattern 4 revisit

--- PatternPrinting/Pattern4.js
+++ PatternPrinting/Pattern4.js
@@ -7,23 +7,20 @@
 
 */
 
-function Pattern4 (n){
- 
-    for(let row = 1 ; row <=n ; row++){
-        let str="";
-        let spaces = n-row;
-     
-        for(let col = 1; col<=spaces ; col++){
-            str+=" ";
-        }
-        let stars = 2*row - 1;
-
-        for(let col = 1; col <= stars; col++){
-            str+="*"
-        }
-        console.log(str);
+function Pattern4(n) {
+  for (let row = 1; row <= n; row++) {
+    let str = "";
+    let spaces = n - row;
+    for (let col = 1; col <= spaces; col++) {
+      str += " ";
+    }
+    let stars = 2 * row - 1;
+    for (let col = 1; col <= stars; col++) {
+      str += "*";
     }
 
+    console.log(str);
+  }
 }
 
-Pattern4(5)
+Pattern4(5);
