89cc52995f7f57c4842e55dd1e1e6db0600ea134
Anmir12 <skannu30@gmail.com>
2026-03-13 23:54:57 +0530

    revisit to this problem

--- Loops/CountDigits.js
+++ Loops/CountDigits.js
@@ -2,47 +2,20 @@
 
 //57896 / 10 => quoitient =5789, reaminder =6  => similarly q=578,r=9 => q=57,r=8 => q=5, r=7
 
-//sum all the reaminders to a result 
+//sum all the reaminders to a result
 
 let countDigit = 57896;
 
-const countDigits = (num) =>{
-    let count = 0;
+const countDigits = (num) => {
+  let count = 0;
 
-    while(num>0){
-        num = Math.floor(num / 10); // 57896 = 57896/10 =>5789/10 =>578/10 =>57/10 =>5/10 => 0
-        
-        count++;
-    }
+  while (num > 0) {
+    num = Math.floor(num / 10); // 57896 = 57896/10 =>5789/10 =>578/10 =>57/10 =>5/10 => 0
 
-    return count;
-}
+    count++;
+  }
+
+  return count;
+};
 
 console.log(countDigits(7654393));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
