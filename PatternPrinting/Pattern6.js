67bbad0bb93410812a27abbf7a653bd9854d5974
Anmir12 <skannu30@gmail.com>
2026-03-13 23:42:54 +0530

    pattern 6 revisit

--- PatternPrinting/Pattern6.js
+++ PatternPrinting/Pattern6.js
@@ -9,41 +9,39 @@
 
 */
 
-function upperTriangle(n){
-    for(let row=1; row<=n; row++){
-        let str='';
-        let spaces=n-row;
-     for(let col=1; col<=spaces;col++){
-        str+=" ";
-     }
-     let stars = 2*row-1;
-     for(let col=1; col<=stars; col++){
-        str+="*"
-     }
-     console.log(str);
-    }
-}
-
-
-function lowerTriangle(n){
-for(let row=1; row<=n; row++){
+function upperTriangle(n) {
+  for (let row = 1; row <= n; row++) {
     let str = "";
-    let spaces = row;
-    for(let col = 1; col<=spaces; col++){
-        str+=" ";
+    let spaces = n - row;
+    for (let col = 1; col <= spaces; col++) {
+      str += " ";
     }
-    let stars = 2*(n-row)-1;
-    for(let col = 1; col<=stars; col++){
-        str+="*";
+    let stars = 2 * row - 1;
+    for (let col = 1; col <= stars; col++) {
+      str += "*";
     }
     console.log(str);
-}
+  }
 }
 
-function Pattern6(n){
-    upperTriangle(n);
-    lowerTriangle(n);
+function lowerTriangle(n) {
+  for (let row = 1; row <= n; row++) {
+    let str = "";
+    let spaces = row;
+    for (let col = 1; col <= spaces; col++) {
+      str += " ";
+    }
+    let stars = 2 * (n - row) - 1;
+    for (let col = 1; col <= stars; col++) {
+      str += "*";
+    }
+    console.log(str);
+  }
+}
 
+function Pattern6(n) {
+  upperTriangle(n);
+  lowerTriangle(n);
 }
 
 Pattern6(8);
