d452b831a20855a53af8003afd27133507921cfe
Anmir12 <skannu30@gmail.com>
2026-03-15 22:40:17 +0530

    revisit to this problem

--- Loops/Fibonacci.js
+++ Loops/Fibonacci.js
@@ -4,15 +4,13 @@ let fib = function(n) {
     if(n < 2){
         return n;
     }
-
-    let prev = 0, curr = 1, next;
-    for(let i=2; i<= n; i++){
-        next = prev + curr;
-        prev = curr;
-        curr = next;
-        console.log(curr);
-    }
-    return next;
+   let prev = 0 ,current = 1, next;
+  for(let i=2; i<=n; i++){
+    next = prev + current;
+    prev = current;
+    current = next;
+  }
+  return next;
 };
 
 // Fibonacci Sequence: 0 1 1 2 3 5 8...
