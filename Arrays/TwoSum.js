ca6b3fdc75f99dd76e933d16a022aed5c46369a4
Anmir12 <skannu30@gmail.com>
2026-03-15 22:50:12 +0530

    revisit this problem

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -1,15 +1,16 @@
-const twoSum =(nums,target) =>{
-    if(!nums.length && target<0){
-        return false;
+const twoSum = (nums, target) => {
+  if (!nums.length && target < 0) {
+    return false;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return true;
+      }
     }
-    for(let i=0; i<nums.length ; i++){
-        for(let j= i+1 ;j<nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i,j];
-            }
-        }
-    }
-    return -1;
-}
+  }
 
-console.log(twoSum([2,3,7,9],5));
+  return false;
+};
+
+console.log(twoSum([2, 4, 7, 9], 5));
