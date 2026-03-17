2b02111fd02296be02e85db15bf6fc123460746f
Anmir12 <skannu30@gmail.com>
2026-03-17 23:18:48 +0530

    revisit to string anagram problem

--- Arrays/StringAnargam.js
+++ Arrays/StringAnargam.js
@@ -1,7 +1,7 @@
 //A String Is Said To Anargam if It Satisfies Below Two Conditions
 //1.The Length Of First String And Second String Need To Be Equal
 //2.The Occurence Of Each Character Should Be Same In Both The Strings
-//Given Are The Examples Of String Anargam 
+//Given Are The Examples Of String Anargam
 //"anmir"="miran"  vaild Anargam
 //"shamir"="mirsha" valid Anargam
 //"Subhani"="anusbhh" invalid Anargam
@@ -25,30 +25,29 @@
 
 //but in Interviews We Maybe Need To Write The Logic Without Using Inbuilt JS Methods
 
-
 function isAnargam(string1, string2) {
   if (string1.length !== string2.length) {
     return false;
   }
-  counter = {};
-  for (let letters of string1) {
-    counter[letters] = (counter[letters] || 0) + 1;
-    // console.log(counter[letters])
+
+  let characters = {};
+
+  for (letter of string1) {
+    characters[letter] = (characters[letter] || 0) + 1;
   }
-  console.log(counter);
-  for (let items of string2) {
-    if (!counter[items]) {
+
+  for (word of string2) {
+    if (!characters[word]) {
       return false;
     }
-    counter[items] -= 1;
+    characters[word] -= 1;
   }
   return true;
 }
 
-let check1 =isAnargam("anmeer","ramene")
-console.log(check1)
-let check2 =isAnargam("shamir","mirsha")
-let check3 =isAnargam("subhani","usbhnii")
-console.log(check1)
-console.log(check2)
-console.log(check3)
+let check1 = isAnargam("anmeer", "ramene");
+let check2 = isAnargam("shamir", "mirsha");
+let check3 = isAnargam("subhani", "usbhnii");
+console.log(check1);
+console.log(check2);
+console.log(check3);
