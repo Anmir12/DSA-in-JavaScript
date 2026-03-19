be54b6a51938041287a3d0a7307dda67433217a9
Anmir12 <skannu30@gmail.com>
2026-03-19 17:14:50 +0530

    solved remove duplicates in sorted array prob

---
+++ Arrays/RemoveDuplicates.js
@@ -0,0 +1,22 @@
+//for a given array [0, 0, 1, 1, 1, 2, 2, 3, 3 ,4] of integers in non-decreasing order remove the duplicates in-place and return the count of elements
+
+//the final array will be like [0,1,2,3,4,_,_,] like this. so we can use two pointer technique here
+
+//one pointer will iterate through all the elements in the array and second one will be used to modify the elements in place
+
+function removeDuplicates(array){
+  let x=0;
+  for(let i=0; i<array.length; i++){
+    if(array[i] !== array[x]){
+        x = x + 1;
+        array[x] =array[i];
+      
+    }
+}
+return x;
+}
+
+const arr = [0,0,1,1,1,2,2,3,3,4]
+const result =removeDuplicates(arr);
+
+console.log(result)
