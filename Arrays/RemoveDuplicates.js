dd1f45bc597c25b81d892060c07e684564218ffb
Anmir12 <skannu30@gmail.com>
2026-03-19 17:16:28 +0530

    handled edgecases

--- Arrays/RemoveDuplicates.js
+++ Arrays/RemoveDuplicates.js
@@ -5,6 +5,9 @@
 //one pointer will iterate through all the elements in the array and second one will be used to modify the elements in place
 
 function removeDuplicates(array){
+    if(arr.length === 0){
+        return 0;
+    }
   let x=0;
   for(let i=0; i<array.length; i++){
     if(array[i] !== array[x]){
@@ -13,7 +16,7 @@ function removeDuplicates(array){
       
     }
 }
-return x;
+return x + 1;
 }
 
 const arr = [0,0,1,1,1,2,2,3,3,4]
