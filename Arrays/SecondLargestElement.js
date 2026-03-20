133736d1cfc2a541269ba4d5253017e1a53e2fdf
Anmir12 <skannu30@gmail.com>
2026-03-20 23:25:42 +0530

    second largest element in arr

---
+++ Arrays/SecondLargestElement.js
@@ -0,0 +1,12 @@
+//given an array of integers [2,7,3,5,9,13,1] find second largest element in it and return it
+
+//solution should handle negative integers and in case of empty array should return -1
+
+
+function secondLargest(arr){
+  
+}
+
+const array = [2,7,3,5,9,13,1];
+
+const result = secondLargest(array)
