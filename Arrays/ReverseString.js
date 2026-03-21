9515473c8940b7f9b3f0b443c26bc4db403579fb
Anmir12 <skannu30@gmail.com>
2026-03-21 23:17:49 +0530

    approach for reverse string prob

---
+++ Arrays/ReverseString.js
@@ -0,0 +1,17 @@
+//for a given array of strings reverse the string in place with O(1) time complexity
+
+//we can use swap technique here and for n length array we can loop till n/2 and replace it
+
+//for example:- (0,n-length-1) ,(1,n.length-2),(2,n.length-3) so it will be likw (i[0], n.length-1-i) ,(i[1], n.length-1-i), (i[2], n.length-1-i)
+
+function ReverseString(str){
+
+for(let i=0 ; i<str.length; i++){
+    
+    let temp = str[i]
+}
+
+}
+
+const stringArr = ["h","e","l","l","o"]
+const result = ReverseString(stringArr)
