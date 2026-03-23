04ef06b472c1546ab3c2cdbfba01f2d84e7c5487
Anmir12 <skannu30@gmail.com>
2026-03-24 00:00:29 +0530

    moveZeros solution

--- Arrays/MoveZeros.js
+++ Arrays/MoveZeros.js
@@ -6,3 +6,24 @@
 
 // Input: nums = [0,1,0,3,12]
 // Output: [1,3,12,0,0]
+
+function MoveZeros (arr){
+ let x = 0;
+
+ for(let i=0; i<arr.length; i++){
+    if(arr[i] !== 0){
+     arr[x] = arr[i];
+     x++;
+    }
+ }
+for (let i=x; i<arr.length; i++){
+    arr[i] = 0;
+}
+ return arr;
+}
+
+const nums = [0,1,0,3,12];
+
+const result = MoveZeros(nums);
+
+console.log(result)
