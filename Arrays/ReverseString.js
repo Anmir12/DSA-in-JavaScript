23bb5202764a19e48f553e8f35a3ff8c8d480d3c
Anmir12 <skannu30@gmail.com>
2026-03-26 22:52:12 +0530

    formatting

--- Arrays/ReverseString.js
+++ Arrays/ReverseString.js
@@ -4,28 +4,27 @@
 
 //for example:- (0,n-length-1) ,(1,n.length-2),(2,n.length-3) so it will be likw (i[0], n.length-1-i) ,(i[1], n.length-1-i), (i[2], n.length-1-i)
 
-function ReverseString(str){
-if(!str.length){
+function ReverseString(str) {
+  if (!str.length) {
     return false;
-}
- let length = str.length;
- let temp;
- let halfLen = Math.floor(length/2)
-for(let i=0 ; i<halfLen; i++){
-    temp = str[length-1-i];
-     str[length-1-i] =str[i];
+  }
+  let length = str.length;
+  let temp;
+  let halfLen = Math.floor(length / 2);
+  for (let i = 0; i < halfLen; i++) {
+    temp = str[length - 1 - i];
+    str[length - 1 - i] = str[i];
     str[i] = temp;
-}
-return str;
+  }
+  return str;
 }
 
-const stringArr1 = ["h","e","l","l","o"];
-const stringArr2 = ["A","n","m","i","r"];
+const stringArr1 = ["h", "e", "l", "l", "o"];
+const stringArr2 = ["A", "n", "m", "i", "r"];
 
 const result1 = ReverseString(stringArr1);
 const result2 = ReverseString(stringArr2);
 
-
-console.log(result1)
-console.log(result2)
+console.log(result1);
+console.log(result2);
 
