314839c7fb0d0513a7e412451a1749b0bbc46af4
Anmir12 <skannu30@gmail.com>
2026-04-06 16:44:16 +0530

    solution for approach 2

--- Arrays/MergeSorted2.js
+++ Arrays/MergeSorted2.js
@@ -34,5 +34,29 @@
 
 // Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 
-
 //without extra Space we are going to solve this problem
+
+function mergeSorted2(nums1, m, nums2, n) {
+  let p1 = m - 1;
+
+  let p2 = n - 1;
+
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) {
+      break;
+    }
+    if (nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1];
+      p1--;
+    } else {
+      nums1[i] = nums2[p2];
+      p2--;
+    }
+  }
+
+  return nums1;
+}
+
+const result = mergeSorted2([1, 2, 3], 3, [2, 5, 6], 3);
+
+console.log(result);
